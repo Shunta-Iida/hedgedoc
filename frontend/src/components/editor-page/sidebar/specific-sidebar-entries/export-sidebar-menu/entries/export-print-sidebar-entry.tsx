d726e6d94e388af90281b0ffaffedf757fa79f1c
Philip Molares <philip.molares@udo.edu>
2024-11-01 17:47:29 +0100

    feat: add print button to export sidebar
    As we want to support the browser print dialog why not make it possible
    to access that from our UI?
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

---
+++ frontend/src/components/editor-page/sidebar/specific-sidebar-entries/export-sidebar-menu/entries/export-print-sidebar-entry.tsx
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2024 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { cypressId } from '../../../../../../utils/cypress-attribute'
+import { SidebarButton } from '../../../sidebar-button/sidebar-button'
+import React, { useCallback } from 'react'
+import { PrinterFill as IconPrinterFill } from 'react-bootstrap-icons'
+import { Trans } from 'react-i18next'
+
+/**
+ * Editor sidebar entry for exporting the markdown content into a local file.
+ */
+export const ExportPrintSidebarEntry: React.FC = () => {
+  const onClick = useCallback(() => {
+    window.print()
+  }, [])
+
+  return (
+    <SidebarButton {...cypressId('menu-export-print')} onClick={onClick} icon={IconPrinterFill}>
+      <Trans i18nKey={'editor.export.print'} />
+    </SidebarButton>
+  )
+}
